7e8ee61c6ecf5b54ceec7d80af7783cc03e73675
gk95 <35295987+gk95@users.noreply.github.com>
2021-12-03 12:41:37 +0000

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,18 @@
 phonebook = {"John" : 938477566, "Jack" : 938377264,"Jill" : 947662781}
 for name, number in phonebook.items():
     print("Phone number of %s is %d" % (name, number)
+
+# Delete John
+del phonebook["John"]
+# Another way to delete 
+# phonebook.pop("John")
+          
+print(phonebook)
+# Add "Jake" to the phonebook with the phone number 938273443, and remove Jill from the phonebook.
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.")  
